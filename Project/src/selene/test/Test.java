600eebae7296a3452d49395f81e1e8d25458dda7
simon <simon.jambu@gmail.com>
2013-04-20 16:19:58 +0200

    static void main

--- Project/src/selene/test/Test.java
+++ Project/src/selene/test/Test.java
@@ -1,10 +1,7 @@
 package selene.test;
 
 public class Test {
-	void toto(int test){
-		int bla = test;
-		bla +=2;
-		if(bla >=3)
-			bla = 0;
+	public static void main(String [] args) {
+		System.out.println("Hello");
 	}
 }
